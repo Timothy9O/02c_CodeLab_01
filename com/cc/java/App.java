75ac847e731fa318190034803bca4baf19d3d6fd
Timothy90 <thrajewitz@gmail.com>
2026-01-28 10:45:31 +0100

    Aufgabe_Unvollständig

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -1,6 +1,32 @@
 package com.cc.java;
 
 public class App {
+
+        public static void updateKontostand(int kontostand) {
+        
+        Konto kontostand = new Konto(10000);
+        Konto kontostand = new konto(10000);
+        Konto kontostand = new konto(10000);
+
+        System.out.println("updated kontostand: " + Kontostand)
+        System.out.println()
+
+
+
+
+
+        Konto.kontostand = Konto1.kontostand *2; // verdopllung
+        Konto.kotostand = Konto2.kontostand *3; // verdreifachung
+        Konto.kontostand = Konto.kontostand *10; // verzehnfachung
+
+        private static void output(String outputStr) {
+            System.out.println(Kontostand1 + kontostand2 + kontostand3);
+        }
+
+}
+
+
+public class App {
     
     public static void main(String[] args) {
     
---
+++ com/cc/java/Konto.java
@@ -0,0 +1,15 @@
+package com.cc.java;
+
+public class Konto {
+
+   private int kontostand = 100000;
+    
+    public Konto (int kontostand) {
+        this 
+    }
+
+    @Override
+    public String toString() {
+        return "Konto []";
+    }
+}
